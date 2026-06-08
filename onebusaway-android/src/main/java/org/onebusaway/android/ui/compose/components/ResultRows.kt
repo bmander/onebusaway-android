b332fb6b09b516a4feba1649340bca170696bc3a
Brandon Martin-Anderson <badhill@gmail.com>
2026-06-07 17:34:23 -0700

    Add a RouteRowContent @Preview for the search/route result row

--- onebusaway-android/src/main/java/org/onebusaway/android/ui/compose/components/ResultRows.kt
+++ onebusaway-android/src/main/java/org/onebusaway/android/ui/compose/components/ResultRows.kt
@@ -18,8 +18,10 @@ package org.onebusaway.android.ui.compose.components
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -30,9 +32,11 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.onebusaway.android.R
+import org.onebusaway.android.ui.compose.theme.ObaTheme
 import org.onebusaway.android.util.UIUtils
 
 /**
@@ -107,6 +111,25 @@ fun RouteRowContent(shortName: String, longName: String?, modifier: Modifier = M
                 color = MaterialTheme.colorScheme.onSurfaceVariant,
                 modifier = Modifier.weight(1f)
             )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun RouteRowContentPreview() {
+    val rowModifier = Modifier
+        .fillMaxWidth()
+        .padding(horizontal = 16.dp, vertical = 12.dp)
+    ObaTheme {
+        Column {
+            RouteRowContent("8", "Seattle Center - Capitol Hill - Rainier Beach", rowModifier)
+            HorizontalDivider()
+            RouteRowContent("12", "Interlaken Park - Capitol Hill - Downtown Seattle", rowModifier)
+            HorizontalDivider()
+            RouteRowContent("225", "Sheridan Park", rowModifier)
+            HorizontalDivider()
+            RouteRowContent("40", null, rowModifier)
         }
     }
 }
